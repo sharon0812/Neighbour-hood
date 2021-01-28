27a9d0321a4b7257ba5f615b25eeaf58e9c79b18
sharon0812 <anyangosharon@gmail.com>
2021-01-28 12:32:52 +0300

    modify forms.py

---
+++ neibour/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile,Neighbourhood,Business,Post
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=60)
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['profile_pic', 'bio']
+
+
+class NewHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
+
+class EditHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
+
+class NewBizForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user',)
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user',)
